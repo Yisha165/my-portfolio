5b8248cccd387ba793eda2e78ed33c7f532614e6
Yisha165 <133154410+Yisha165@users.noreply.github.com>
2023-05-29 23:01:56 +0300

    made changes

--- java.js
+++ java.js
@@ -1,9 +1,43 @@
-let container = document.querySelector(".greeting");
-let timeNow =new Date().getHours();
-let greeting =
-timeNow >= 5 && timeNow < 12
-? "Good Morning"
-: timeNow >= 12 && timeNow < 18
-? "Good Afternoon"
-: "Good Evening";
-greeting.innerHTML = `<h1> ${greeting}</h1>`;
+
+   
+   var j = 0; 
+   var images = [];
+   var time = 3000;
+   
+   images[0] = "purpleflower.jpg";
+   images[1] = "pinkflower.jpg";
+   images[2] = "roses.jpg"
+   
+   function changeImg(){
+       document.slide.src = images[j];
+       if(j < images.length -1){
+          j++; 
+       }
+       else
+       j= 0;
+   
+   setTimeout("changeImg()", time); }
+   
+   window.onload = changeImg;
+   //Greeting feature
+       document.addEventListener("DOMContentLoaded", function()
+       {
+           var currentDate = new Date();
+           var currentHour =currentDate.getHours();
+           var greeting;
+           if(currentHour >= 0 && currentHour <=12){
+               greeting ="Good Morning";
+           } else if(currentHour >= 12 && currentHour <= 18){
+               greeting= "Good Afternoon";
+           } else {
+               greeting ="Good Evening";
+           }
+           var greetings = document.getElementById("para1");
+           greetings.textContent = greeting;
+       });
+       
+       dots[i].className.replace(" active", "");
+    
+     slides[slideIndex-1].style.display = "block";  
+     dots[slideIndex-1].className += " active";
+
