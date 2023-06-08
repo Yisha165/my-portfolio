10380c0c9b88eb46064e0606157919411fb72a70
Yisha165 <133154410+Yisha165@users.noreply.github.com>
2023-06-08 22:32:32 +0300

    php was added

---
+++ my login.php
@@ -0,0 +1,12 @@
+<?php
+$severname="localhost";
+$username="root";
+$password=""
+$dbname="data";
+$conn=mysqli_connect($servername,$username, $password,$dbname);
+if(!$conn){
+    die("error here".mysqli_connect_error());
+}else{
+echo"success";
+}
+?>
